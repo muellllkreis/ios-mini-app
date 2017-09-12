587b79fcc38c1f62852be9a3d0c65bedc285d730
Matias Rietig <mjr9r@labmini10.lab.cs.virginia.edu>
2017-09-12 18:39:24 -0400

    Initial Commit

---
+++ iOS BucketList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS BucketList
+//
+//  Created by Matias Rietig on 9/12/17.
+//  Copyright © 2017 opal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
